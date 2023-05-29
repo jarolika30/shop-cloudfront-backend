c0430d50e6d5e95b476d0e0341ea9c3e948b92de
Katsiaryna Staurova <Katsiaryna_Staurova@epam.com>
2023-05-29 09:04:36 +0300

    implement task 6

--- import-service/importFileParser/handler.js
+++ import-service/importFileParser/handler.js
@@ -1,44 +1,68 @@
 'use strict';
 const csvParser = require('csv-parser');
 const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
+const { SQSClient, SendMessageCommand } = require('@aws-sdk/client-sqs');
 
 const s3Client = new S3Client({ region: 'us-east-1' });
+const sqsClient = new SQSClient({ region: 'eu-central-1' });
+const url =
+  'https://sqs.us-east-1.amazonaws.com/241918472493/catalogItemsQueue';
+
+const sendToSqs = async (data) => {
+  console.log(data);
+
+  const params = {
+    QueueUrl: url,
+    MessageBody: JSON.stringify(data)
+  };
+
+  console.log(`Sending SQS Command`);
+
+  try {
+    await sqsClient.send(new SendMessageCommand(params));
+    console.log(`${data.length} items sent to SQS!`);
+  } catch (error) {
+    console.log(error.message);
+    throw error;
+  }
+};
 
 module.exports.importFileParser = async (event) => {
   console.log(event);
 
-  const productsData = [];
   const params = {
     Bucket: 'import-service-staurova',
     Key: `uploaded/products.csv`
   };
 
   try {
-    const command = new GetObjectCommand(params);
-    const s3Stream = await s3Client.send(command);
+    const productsData = [];
+    const getObjectCommand = new GetObjectCommand(params);
+    const s3Stream = await s3Client.send(getObjectCommand);
+    const csvParser = s3Stream.Body.pipe(
+      csv({ delimiter: ',', columns: true })
+    );
 
-    s3Stream.Body.pipe(csvParser())
-      .on('data', (data) => {
-        console.log(data);
-        productsData.push(data);
+    const promises = [];
+
+    promises.push(
+      new Promise((resolve) => {
+        csvParser.on('data', (data) => {
+          productsData.push(data);
+        });
+        csvParser.on('end', resolve);
       })
-      .on('error', () => {
-        return {
-          statusCode: 400,
-          body: JSON.stringify({ message: `Failed to read data!` })
-        };
-      })
-      .on('end', () => {
-        console.log(productsData);
-        return {
-          statusCode: 202,
-          body: JSON.stringify({ message: `Successfully read and parsed!` })
-        };
-      });
+    );
+
+    await Promise.all(promises);
+
+    try {
+      await sendToSqs(productsData);
+    } catch (error) {
+      console.log(`Error: ${error.message}`);
+    }
   } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify(`Internal Server Error: ${error.message}`)
-    };
+    console.log(error.message);
+    throw error;
   }
 };
